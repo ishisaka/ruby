17d6292f5aac7ca85064d8628d0ff70b271c8d1f
Tadahiro Ishisaka <tishisaka@baleen.studio>
2023-12-20 09:54:18 +0000

    2.8.3ヒアドキュメント途中

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -17,3 +17,27 @@ puts fizz_buzz(4)
 puts fizz_buzz(5)
 puts fizz_buzz(6)
 puts fizz_buzz(15)
+
+
+a = <<TEXT
+これはヒアドキュメントです。
+複数行に渡る長い文字列を作成するのに便利です。
+TEXT
+puts a
+
+
+def some_method2
+    <<-TEXT
+    これはヒアドキュメントです。
+    <<-を使うと最後の識別子をインデントさせることができます。
+    TEXT
+end
+
+def some_method3
+    <<~TEXT 
+    \ 各行の行頭に半角スペースを2文字入れます。
+    \ このとき行頭はバックスラッシュで指定します。
+    TEXT
+end
+
+
